2669136459a5d53380c106a62cfcf385b3eb278e
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-28 22:55:06 -0400

    Locomotion setup

--- tread.py
+++ tread.py
@@ -1,4 +1,12 @@
 # Class for transportation
 
 def locomotion:
+  def __init__(self, speed, powerCost, turnSpeed) :
+    self.speed = speed
+    self.powerCost = powerCost
+    self.turnSpeed = turnSpeed
+  def __str__(self):
+    return "Locomotion"
 
+
+def wheels:
