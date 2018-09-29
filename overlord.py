f9d3aa28f2f50c100ca72beb363371a7d8e0f187
Ruethus <redingtonknoxtn@gmail.com>
2018-09-28 21:47:06 -0400

    Added name to window title

--- overlord.py
+++ overlord.py
@@ -7,25 +7,20 @@ bg=pygame.Surface(scr.get_size())
 bg.fill((127,0,0))
 bg=bg.convert()
 scr.blit(bg,(0,0))
-
+clk = pygame.time.Clock()
 mainloop=True
+pygame.display.set_caption("Zoned For War")
 
 while mainloop:
-    #milliseconds = clock.tick(FPS) 
-	#playtime += milliseconds / 1000.0 
-    
-    for event in pygame.event.get():
-        # User presses QUIT-button.
-        if event.type == pygame.QUIT:
-            mainloop = False 
-        elif event.type == pygame.KEYDOWN:
-            # User presses ESCAPE-Key
-            if event.key == pygame.K_ESCAPE:
-                mainloop = False
-                
-    # Print framerate and playtime in titlebar.
-   # text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps,playtime)
-    #pygame.display.set_caption(text)
-
-    #Update Pygame display.
-    pygame.display.flip()
+ milliseconds = clk.tick()
+ for event in pygame.event.get():
+  # User presses QUIT-button.
+  if event.type == pygame.QUIT:
+   mainloop = False 
+  elif event.type == pygame.KEYDOWN:
+   # User presses ESCAPE-Key
+   if event.key == pygame.K_ESCAPE:
+    mainloop = False
+ # Print framerate and playtime in titlebar.
+ #Update Pygame display.
+ pygame.display.flip()
