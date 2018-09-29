3ab433e509247dec774c350b659366e2d5c90bed
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-28 23:41:10 -0400

    Chassis!

--- chassis.py
+++ chassis.py
@@ -1,4 +1,13 @@
 # The center of the Tank
 
+# This is the inheritance version. Please do not use
+# Please make versions below
 def chassis:
+  def __init__(self, primary, secondary, armorBonus, energyCapacity, chargeRate):
+    self.primary = primary
+    self.secondary = secondary
+    self.energyCapacity = energyCapacity
+    self.chargeRate = chargeRate
+  def __str__(self):
+    return "Chassis"
 
