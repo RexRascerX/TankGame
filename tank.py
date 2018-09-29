51904376f11caf2a39b405efec2e22f5868527aa
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-29 12:04:28 -0400

    added turrets to tanks.

--- tank.py
+++ tank.py
@@ -1,11 +1,18 @@
 # The thing you make when a tank apears
-import tread.py, chassis.py
+import tread.py, chassis.py, turrets.py
 
 # User defined tanks
 def tank:
   def __init__(self,locomotion, chassis):
     self.locomotion = locomotion
     self.chassis = chassis
+    self.primaries = []
+    self.secondaries = []
   def __str__(self):
     return "Tank"
-  
+  def addPrimary(turret):
+    if (self.primaries.count() < self.chassis.primary):
+      self.primaries.append(turret)
+  def addSecondary(turret):
+    if (self.secondaries.count() < self.chassis.secondary):
+      self.secondaries.append(turret)
