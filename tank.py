6183b0e923204b919fd31d84de57baa69c9d5d6f
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-29 00:36:21 -0400

    Added bodies to the tanks.

--- tank.py
+++ tank.py
@@ -1,5 +1,11 @@
 # The thing you make when a tank apears
+import tread.py, chassis.py
 
+# User defined tanks
 def tank:
-  def __init__(self,locomotion):
+  def __init__(self,locomotion, chassis):
     self.locomotion = locomotion
+    self.chassis = chassis
+  def __str__(self):
+    return "Tank"
+  
