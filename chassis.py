709968c9dd24327e535b5e30a8335d89a15a796c
Lucas Sieh <plutoniumonkey@gmail.com>
2018-09-29 09:38:44 -0400

    Error fix

--- chassis.py
+++ chassis.py
@@ -6,6 +6,7 @@ def chassis:
   def __init__(self, primary, secondary, armorBonus, energyCapacity, chargeRate):
     self.primary = primary
     self.secondary = secondary
+    self.armorBonus = armorBonus
     self.energyCapacity = energyCapacity
     self.chargeRate = chargeRate
   def __str__(self):
