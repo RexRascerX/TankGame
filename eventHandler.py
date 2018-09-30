8bfa09b2f03d600ddf114cd8673d909719c1369e
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-30 00:04:51 -0400

    Handler queue fix again

--- eventHandler.py
+++ eventHandler.py
@@ -4,17 +4,17 @@ class eventOV:
   queue = []
   exit = False
   def __init__(self):
-    pg = pygame
+    None
   def __str__(self):
     return "Why? Just why have you done this? Are you happy now?"
   def check(self):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
-        exit = True
+        eventOV.exit = True
       if event.type == pygame.KEYDOWN:
-        queue.append(event.key)
+        eventOV.queue.append(event.key)
       if event.type == pygame.KEYUP:
-        queue.remove(event.key)
+        eventOV.queue.remove(event.key)
     print(eventOV.queue)
   def noKeys():
     return len(queue) == 0
