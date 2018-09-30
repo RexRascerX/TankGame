a047057bdfcb8e14c1e848e1b1b7527f15fe199e
Lucas Sieh <plutoniumonkey@gmail.com>
2018-09-30 04:58:32 -0400

    Ok

--- tank.py
+++ tank.py
@@ -22,17 +22,17 @@ class tank:
     return "Tank"
 #Needs to be run if you want weapons
   def addPrimary(self, turret):
-    if (self.primaries.count() < self.chassis.primary):
+    if len(self.primaries) < self.chassis.primary:
       self.primaries.append(turret)
       self.primaryAs.update(turret,self.a)
     else:
-      print("Denied Primary " + turret)
+      print("Denied Primary " + str(turret))
   def addSecondary(self, turret):
-    if (self.secondaries.count() < self.chassis.secondary):
+    if len(self.secondaries) < self.chassis.secondary:
       self.secondaries.append(turret)
       self.secondaryAs.update(turret,self.a)
     else:
-      print("Denied Secondary " + turret)
+      print("Denied Secondary " + str(turret))
 
 #This calculates basic stats
   def getPowerTotal(self):
