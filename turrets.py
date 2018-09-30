1a75ccaf741079eafd04d28777563464c450f066
Lucas Sieh <plutoniumonkey@gmail.com>
2018-09-30 04:03:00 -0400

    Fixed

--- turrets.py
+++ turrets.py
@@ -13,7 +13,7 @@ class turret:
         -self.energyConsumption
     def type(self):
         return 0;
-    def render(self):
+    def primaryRender(self):
       pass
 
 class passiveTurret(turret):
@@ -27,7 +27,7 @@ class passiveTurret(turret):
       pass
     def matter(self):
       pass
-    def render(self):
+    def primaryRender(self):
       pass
 
 class aggressiveTurret(turret):
@@ -37,7 +37,7 @@ class aggressiveTurret(turret):
         return "Aggressive Turret"
     def type(self):
         return -1;
-    def render(self):
+    def primaryRender(self):
       pass
 
 class matter(aggressiveTurret):
@@ -46,7 +46,7 @@ class matter(aggressiveTurret):
         super().__init__(sightLength, turningSpeed, energyConsumption, armorBonus)
     def __str__(self):
         return "Matter based turret"
-    def render(self):
+    def primaryRender(self):
       pass
         
 class energy(aggressiveTurret):
@@ -54,54 +54,73 @@ class energy(aggressiveTurret):
         super().__init__(sightLength, turningSpeed, energyConsumption, armorBonus)
     def __str__(self):
         return "Energy based turret"
-    def render(self):
+    def primaryRender(self):
       pass
 
 class GaussRifle(matter):
     def __init__(self, level):
+        self.length = 19
+        self.width = 8
         super().__init__(10+level, 2, 1+(level/3), 3, 3)
     def __str__(self):
         return "Gauss Rifle"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,19,0x10ff10))
-
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length,self.width,0x10ff10))
+    def secondaryRender(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)
 class Type67(matter):
     def __init__(self, level):
+        self.length = 14
+        self.width = 8
         super().__init__(3+(level/4), 8+(level/2), 0.1+(level/5), 7, 1)
     def __str__(self):
         return "Type 67"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,14,0x10ff10))
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length,self.width,0x10ff10))
+    def secondaryRender(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)r
         
 class BigBertha(matter):
     def __init__(self, level):
+        self.length = 16
+        self.width = 8
         super().__init__(6+level, 3+(level/2), 3+(level/2), 5, 5)
     def __str__(self):
         return "Big Bertha"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,16,0x10ff10))
-    
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length,self.width,0x10ff10))
+    def secondaryRender(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)
 class ElectronNeedler(energy):
     def __init__(self, level):
+        self.length = 19
+        self.width = 8
         super().__init__(10+level, 2, 5+(level/2), 3)
     def __str__(self):
         return "Electron Needler"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,19,0xff00ff))
-    
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length, self.width, 0xff00ff))
+    def secondaryRender(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)
 class ProtonBlaster(energy):
     def __init__(self, level):
+        self.length = 14
+        self.width = 8
         super().__init__(3+(level/4), 8+(level/2), 0.4+(level/3), 7)
     def __str__(self):
         return "Proton Blaster"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,14,0xff00ff))
-    
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length, self.width,0xff00ff))
+    def secondaryRender(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)
 class NeutronNeuterer(energy):
     def __init__(self, level):
+        self.length = 16
+        self.width = 8
         super().__init__(6+level, 3+(level/2), 5+level, 5)
     def __str__(self):
         return "Neutron Neuterer"
-    def render(self, x, y, a):
-      return imaging.rImage(x,y,a,imaging.rRect(8,16,0xff00ff))
-    
+    def primaryRender(self, x, y, a):
+        return imaging.rImage(x,y,a,imaging.rRect(self.length,self.width,0xff00ff))
+    def render(self):
+        return imaging.rRect(self.length,self.width,0xFFFFFF)
