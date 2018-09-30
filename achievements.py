b37691f9f5af4ba2535bb920cbc9720133fddb12
Lucas Sieh <plutoniumonkey@gmail.com>
2018-09-29 23:18:21 -0400

    Added stuff

--- achievements.py
+++ achievements.py
@@ -1,2 +1,6 @@
 #ideas? simple achievements can include one for starting the game, one for getting a first kill, etc
-
+class score:
+    def __init__(self, scoreCounter):
+        self.scoreCounter = scoreCounter
+    def __str__(self):
+        return "Score"
