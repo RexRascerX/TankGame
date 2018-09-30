703de10ca13d85d9bfd94ef6a862d7ee11e551ba
Rex <38085809+RexRascerX@users.noreply.github.com>
2018-09-29 23:25:19 -0400

    Better events

--- eventHandler.py
+++ eventHandler.py
@@ -1,13 +1,14 @@
 import pygame, sys
 
 class eventOV:
+    queue = []
+    pg = None
+    exit = False
   def __init__(self, pygame):
-    self.pg = pygame
-    self.queue = []
-    self.exit = False
+    pg = pygame
   def __str__(self):
     return "Why? Just why have you done this? Are you happy now?"
-  def check():
+  def check(self):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         exit = True
@@ -15,14 +16,7 @@ class eventOV:
         queue.append(event.key)
       if event.type == pygame.KEYUP:
         queue.remove(event.key)
-  def next():
-    if queue.count() > 0:
-      temp = queue[0]
-      queue.pop(0)
-      return temp
-    else
-      return -1
-  def empty():
+  def noKeys():
     return len(queue) == 0
   def end():
     return exit
